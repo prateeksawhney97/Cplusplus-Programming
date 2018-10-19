bebc64ee23b179b1a8e013d824a4ff3f7da0cb19
Prateek Sawhney <Prateek.sawhney97@gmail.com>
2018-10-19 21:14:53 +0530

    Create Basic Data Types.cpp

---
+++ Basic Data Types.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int m;
+    long n;
+    char o;
+    float p;
+    double q;
+    scanf("%d %ld %c %f %lf", &m, &n, &o, &p, &q);
+    printf("%d\n%ld\n%c\n%f\n%lf", m, n, o, p, q);
+    return 0;
+}
